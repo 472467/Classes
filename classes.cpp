5e7b9581c3bbf8c30cf5788071c0bcb11140d453
U-BSD\472467 <472467@SHS-10D8A69E6.beaverton.k12.or.us>
2016-11-01 14:30:20 -0700

    Added some translateMove stuff

--- classes.cpp
+++ classes.cpp
@@ -21,7 +21,7 @@ int main(){
     input[x]= '\0';
   }
   while(true){
-    cout << "Welcome to the student directory. Please 'ADD', 'PRINT'(prints list of ALL students) or 'DELETE' a student. Type EXIT to quit.\n";
+    cout << "Welcome to the media database. Please 'ADD', 'SEARCH'(prints matching Media units) or 'DELETE' a Media unit. Type EXIT to quit the program.\n";
     cin.clear();
     cin.sync();//cleans cin
     cin.getline(input, 30);
@@ -32,8 +32,24 @@ int main(){
 }
 
 int translateMove(char* in, vector<STUDENT*>* v){
+  bool running = true;
+  char newIn [200];
   if(strcasecmp(in, "exit") == 1){
-      
       return(1);
+  }else if(strcasecmp(in, "add") == 1){
+    jump:
+      cout << "What type of media would you like to add? VideoGame(1), Song(2), Movie(3)";
+      int mType = 0;
+      scanf ("%d",&mType);//way to input an int without mixing cin with cin.getline(creates segmentation faults)
+      cout << mType;
+      if(mType != 1 && mType != 2 && mType != 3){
+	goto jump;
+      }else{
+	goto endloop;
+      }
+  endloop:
   }
+      
+  
+
 }
